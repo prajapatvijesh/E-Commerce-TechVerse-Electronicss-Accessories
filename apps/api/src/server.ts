0f237e42b7c80b3cfe1a9b3a74b022e929298066
Prajapat Vijesh <prajapatvijesh892@gamil.com>
2026-07-17 13:45:21 +0530

    Allow all CORS origins for demo

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -14,7 +14,7 @@ const app: Express = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: (origin, callback) => callback(null, true),
     credentials: true,
   },
 });
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: (origin, callback) => callback(null, true),
   credentials: true,
 }));
 app.use(morgan('dev'));
